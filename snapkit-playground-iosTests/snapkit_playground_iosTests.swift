fd940d05f741e16a1f5bd539c57f4477c467a471
Josip Rezic <josip.rezic@hotmail.com>
2020-07-05 12:05:59 +0200

    Add project

---
+++ snapkit-playground-iosTests/snapkit_playground_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  snapkit_playground_iosTests.swift
+//  snapkit-playground-iosTests
+//
+//  Created by Josip Rezić on 7/5/20.
+//  Copyright © 2020 Josip Rezić. All rights reserved.
+//
+
+import XCTest
+@testable import snapkit_playground_ios
+
+class snapkit_playground_iosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
