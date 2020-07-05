d1dfa853793f83db300cc83b198ab1b0ab9f37b0
Josip Rezic <josip.rezic@hotmail.com>
2020-07-05 13:07:22 +0200

    Cleanup

--- snapkit-playground-ios/ContentContainerPreview.swift
+++ snapkit-playground-ios/ContentContainerPreview.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import SnapKit
 
 struct ContentContainerView_Previews: PreviewProvider {
     static var previews: some View { ContentContainerView() }
@@ -14,24 +15,35 @@ struct ContentContainerView_Previews: PreviewProvider {
 
 typealias PreviewView = ContentView
 
+//
+// MARK: - Content view to modify
+//
+
 final class ContentView: BaseView {
     
     private let testLabel = UILabel()
     
     override func setupSubviews() {
+        
         // Your code here ...
+        
         addSubview(testLabel)
     }
     
     override func setupStyling() {
-        // Your code here ...
-        backgroundColor = .green
         
-        testLabel.backgroundColor = .gray
-        testLabel.text = "Test"
+        // Your code here ...
+        
+        testLabel.backgroundColor = .lightGray
+        testLabel.text = "Hello World"
     }
     
     override func setupConstraints() {
+        
         // Your code here ...
+        
+        testLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
 }
